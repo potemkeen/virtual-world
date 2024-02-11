2e73ac94f75fd8827c6755da7d48a7c8f43b8b8e
polumordvinov_v <polumordvinov_v@tanukitech.ru>
2024-02-11 18:57:11 +0300

    graph

---
+++ src/js/math/graph.js
@@ -0,0 +1,69 @@
+export class Graph {
+    constructor(points = [], segments = []) {
+        this.points = points;
+        this.segments = segments;
+    }
+
+    addPoint(point) {
+        this.points.push(point);
+    }
+
+    containsPoint(point) {
+        return this.points.some((p) => p.equals(point));
+    }
+
+    tryAddPoint(point) {
+        if (!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
+    removePoint(point) {
+        const segs = this.getSegmentsWithPoint(point);
+        for (const seg of segs) {
+            this.removeSegment(seg);
+        }
+        this.points.splice(this.points.indexOf(point), 1);
+    }
+
+    addSegment(seg) {
+        this.segments.push(seg);
+    }
+
+    containsSegment(seg) {
+        return this.segments.some((s) => s.equals(seg));
+    }
+
+    tryAddSegment(seg) {
+        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    }
+
+    removeSegment(seg) {
+        this.segments.splice(this.segments.indexOf(seg), 1);
+    }
+
+    getSegmentsWithPoint(point) {
+        return this.segments.filter((seg) => seg.includes(point));
+    }
+
+    dispose() {
+        this.points = [];
+        this.segments = [];
+    }
+
+    draw(ctx) {
+        for (const seg of this.segments) {
+            seg.draw(ctx);
+        }
+
+        for (const point of this.points) {
+            point.draw(ctx);
+        }
+    }
+}
